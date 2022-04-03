e215d5643ea35dcc0908c064e7dae7296d91e3ef
alebarreto <alessandrobarreto50@gmail.com>
2022-04-03 11:27:26 -0400

    :fire: fix error ci

--- features/featureA/src/main/java/com/dev/featurea/presentation/FeatureAActivity.kt
+++ features/featureA/src/main/java/com/dev/featurea/presentation/FeatureAActivity.kt
@@ -3,9 +3,7 @@ package com.dev.featurea.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dev.featurea.R
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class FeatureAActivity : AppCompatActivity(R.layout.activity_feature_a) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
