06f43f87c3848cef24dc569f16d9622480101256
alebarreto <alessandrobarreto50@gmail.com>
2022-04-11 12:21:21 -0400

    :hammer: create android-base-feature plugin and bump gradle versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,8 +2,17 @@ import org.gradle.kotlin.dsl.`kotlin-dsl`
 
 plugins {
     `kotlin-dsl`
+    `java-gradle-plugin`
 }
 
 repositories {
+    google()
     mavenCentral()
+    maven("https://plugins.gradle.org/m2/")
+}
+
+dependencies {
+    implementation("com.android.tools.build:gradle:7.0.4")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
+    implementation("com.google.dagger:hilt-android-gradle-plugin:2.40.5")
 }
--- features/featureA/build.gradle.kts
+++ features/featureA/build.gradle.kts
@@ -1,50 +1,11 @@
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
-    id("kotlin-kapt")
-    id("kotlin-parcelize")
-    id("dagger.hilt.android.plugin")
+    id("commons.android-base-feature")
 }
 
 android {
-    compileSdk = BuildConfig.COMPILE_SDK
-    defaultConfig {
-        minSdk = BuildConfig.MIN_SDK
-        targetSdk = BuildConfig.TARGET_SDK
-        testInstrumentationRunner = BuildConfig.TEST_INSTRUMENTATION_RUNNER
-        consumerProguardFiles("proguard-rules.pro")
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    resourcePrefix = "featureA"
 }
 
 dependencies {
-    implementation(project(Commons.get()))
-
-    AndroidX.loadAll().forEach { implementation(it) }
-
-    implementation(RetrofitConfig.retrofit)
-
-    Google.loadAll().forEach { implementation(it) }
-    kapt(Google.hiltCompiler)
-
-    JUnit.loadAll().forEach { testImplementation(it) }
-    AndroidTest.loadAll().forEach { androidTestImplementation(it) }
+    // module specific dependencies
 }
